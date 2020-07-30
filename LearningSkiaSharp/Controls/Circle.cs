c6f1243932911b7ce46b5748fae3fe98c0cbe865
Aswin P G <aswinpg4444@outlook.com>
2020-07-30 23:59:05 +0530

    Add project files.

---
+++ LearningSkiaSharp/Controls/Circle.cs
@@ -0,0 +1,26 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace LearningSkiaSharp.Controls
+{
+    public class Circle : ContentView
+    {
+        public Circle()
+        {
+            var Canvas = new SKCanvasView();
+            Canvas.PaintSurface += Canvas_PaintSurface;
+        }
+
+        private void Canvas_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            SKSurface sKSurface = e.Surface;
+            SKCanvas sKCanvas = sKSurface.Canvas;
+
+            sKCanvas.Clear(SKColors.Red);
+        }
+    }
+}
